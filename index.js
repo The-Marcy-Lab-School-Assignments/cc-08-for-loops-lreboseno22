b5d00f90d7b2e83827f7420433d5b589a6f0bd0d
lreboseno22 <liamrebo22@gmail.com>
2021-10-26 14:39:33 +0000

    Completed questions

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 //1
 function countToTwenty(){
-  
+  for (let i = 1; i <= 20; i++){
+      console.log(i);
+  }
 }
+// countToTwenty();
 
 //2
 function countFromTwenty(){
-  
+  for (let i = 20; i >= 1; i--){
+      console.log(i);
+  }
 }
+// countFromTwenty();
 
 //3
-function countToN(){
-  
+function countToN(number){
+  for (let i = 1; i <= number; i++){
+      console.log(i);
+  }
 }
-
-
+// countToN(5);
 //4
-function countFromN(){
-  
+function countFromN(number){
+  for (let i = number; i >= 1; i--){
+      console.log(i);
+  }
 }
+// countFromN(10);
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(integer){
+  for (let i = 1; i <= integer; i ++){
+      if (i % 2 !== 0){
+          console.log(i);
+      }
+  }
 }
+countEveryOdd(9);
